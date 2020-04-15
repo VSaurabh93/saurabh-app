53a53ffef08df6a7e3eb08c3f241613cb14e7189
VSaurabh93 <saurabh.uday.93@gmail.com>
2020-04-15 05:30:15 -0400

    Working code

--- index.js
+++ index.js
@@ -19,44 +19,47 @@ async function fetchRandomURL (url){
     return response.json();
 }
 
+
 class ElementHandler {
+  constructor(variant)
+  {
+    this.variant = variant;
+  }
   element(element) {
     // An incoming element, such as `div`
     switch(element.tagName)
     {
       case 'title' : 
-      //console.log(`Incoming title: ${element}`)
-      element.setInnerContent("Updated Variant 1")
+      element.setInnerContent(`Updated Variant ${this.variant}`)
       break;
       case 'h1' : 
-      //console.log(`Incoming h1: ${element.tagName}`)
-      element.setInnerContent("Updated Variant 1")
+      element.setInnerContent(`This is Updated Variant ${this.variant}`)
       break;
       case 'p' : 
-      //console.log(`Incoming p: ${element.tagName}`)
-      element.setInnerContent("Updated Variant 1 - brought to you by Saurabh")
+      element.setInnerContent(`Updated Variant ${this.variant} - brought to you by Saurabh`)
       break;
       case 'a' : 
-      //console.log(`Incoming a: ${element.tagName}`)
-      element.setInnerContent("Go to Saurabh's Github Repository")
-      element.setAttribute("href", "https://github.com/VSaurabh93/");
-      //console.log(element.getAttribute("href"));
+      if(this.variant == 'one')
+      {
+        element.setInnerContent("Go to Saurabh's Github Repository")
+        element.setAttribute("href", "https://github.com/VSaurabh93/");
+      }
+      else if(this.variant == 'two')
+      {
+        element.setInnerContent("Go to Saurabh's LinkedIn Profile")
+        element.setAttribute("href", "https://www.linkedin.com/in/saurabhverma93/");
+      }
       break;
       default:
         break;
     }
-    //console.log(`Incoming element: ${element.tagName}`)
-  }
-
-  comments(comment) {
-    // An incoming comment
-  }
-
-  text(text) {
-    // An incoming piece of text
   }
 }
 
+// getRandom(max)
+// {
+//   return Math.floor(Math.random() * max)
+// }
 
 /**
  * Respond with hello worker text
@@ -67,11 +70,30 @@ async function handleRequest(request) {
   url= "https://cfw-takehome.developers.workers.dev/api/variants"
   //response = fetchRandomURL(url);
   obj = await (await fetch(url)).json();
-  console.log(obj["variants"][0]);
+  //console.log(obj["variants"][0]);
 
-  data = await fetch(obj["variants"][0]);
-  return new HTMLRewriter().on('*', new ElementHandler()).transform(data);
-
+  const cookie = request.headers.get('cookie'); 
+  if (cookie && cookie.includes(`variant=one`)) {
+    let resp = await fetch(obj["variants"][0]);
+    let elementHandler = new ElementHandler("one");
+    return new HTMLRewriter().on('*', elementHandler).transform(resp);
+  } 
+  else if (cookie && cookie.includes(`variant=two`)) {
+    let resp = await fetch(obj["variants"][1]);
+    let elementHandler = new ElementHandler("two");
+    return new HTMLRewriter().on('*', elementHandler).transform(resp);
+  }
+  else
+  {
+    let variant = Math.random() < 0.5 ? "one" : "two" // 50/50 split
+    let index = variant === "one" ? 0 : 1;
+    let resp = await fetch(obj["variants"][index]);
+    let elementHandler = new ElementHandler(variant);
+    response = new HTMLRewriter().on('*', elementHandler).transform(resp);
+    response.headers.append('Set-Cookie', `variant=${variant}; path=/`);
+    return response;
+  }
+  
   //  return new Response(data, {
   //   headers: { 'content-type': 'text/plain' },
   // })
